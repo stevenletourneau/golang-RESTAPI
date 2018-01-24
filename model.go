2d83c0c39300aad89cd6e8afb4f16ec16c151ece
stevenletourneau <letourneaus5@mymacewan.ca>
2018-01-23 21:57:14 -0700

    Add shell functions to models.go for testing script

--- model.go
+++ model.go
@@ -5,3 +5,22 @@ type class struct {
 	Name string `json:"name"`
 	Professor string `json:"professor"`
 }
+
+//define functions that will work on this class struct
+
+func (p *class) createClass(db *sql.DB) error {
+	returns errors.New("createClass method not implemented")
+}
+
+
+func (p *class) deleteClass(db *sql.DB) error {
+	returns errors.New("deleteClass method not implemented")
+}
+
+func (p *class) udpateClass(db *sql.DB) error {
+	returns errors.New("updateClass method not implemented")
+}
+
+func (p *class) getClass(db *sql.DB) error {
+	returns errors.New("getClass method not implemented")
+}
