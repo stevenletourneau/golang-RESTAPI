a7a99a9c19035a00c08f121fe1a209e36dcb9a2c
stevenletourneau <letourneaus5@mymacewan.ca>
2018-01-23 22:01:07 -0700

    Add more methods and imports to models.go for testing script

--- model.go
+++ model.go
@@ -1,5 +1,12 @@
-//struct to represent example class for testing routes
+//model.go
+package main
 
+import (
+	"database/sql"
+	"errors"
+)
+
+//struct to represent example class for testing routes
 type class struct {
 	ID int `json."id"`
 	Name string `json:"name"`
@@ -7,20 +14,23 @@ type class struct {
 }
 
 //define functions that will work on this class struct
-
 func (p *class) createClass(db *sql.DB) error {
-	returns errors.New("createClass method not implemented")
+	returns errors.New("createClass method not implemented\n")
 }
 
-
 func (p *class) deleteClass(db *sql.DB) error {
-	returns errors.New("deleteClass method not implemented")
+	returns errors.New("deleteClass method not implemented\n")
 }
 
 func (p *class) udpateClass(db *sql.DB) error {
-	returns errors.New("updateClass method not implemented")
+	returns errors.New("updateClass method not implemented\n")
 }
 
 func (p *class) getClass(db *sql.DB) error {
-	returns errors.New("getClass method not implemented")
+	returns errors.New("getClass method not implemented\n")
+}
+
+//define function that will fetch all classes
+func getClasses(db *sql.DB, start, count int) ([]class, error) {
+	return nil, errors.New("GetClasses methos not implemented\n")
 }
